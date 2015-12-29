929944976b41f5ee5e1a38da39939529bc4862b5
mr5 <u@mr-5.cn>
2015-12-30 00:37:50 +0800

    add VERSION NUMBER auto replacement

--- aliyun-opensearch.php
+++ aliyun-opensearch.php
@@ -4,7 +4,7 @@ Plugin Name: AliYun Open Search
 Plugin URI: http://www.aliyun.com/product/opensearch/
 Description: Aliyun Open Search is a hosting service for structured data searching. Supporting data structures, sorting and data processing freedom to customize. Aliyun Open Search provides a simple, low cost, stable and efficient search solution for your sites or applications.
 Author: Aliyun
-Version: 0.1
+Version: dev
 Author URI: http://www.aliyun.com/product/opensearch/
 */
 
@@ -21,7 +21,13 @@ if (!defined('WPINC')) {
  */
 require plugin_dir_path(__FILE__) . 'includes/AliyunOpenSearch.php';
 define('ALI_OPENSEARCH_PLUGIN_NAME', 'aliyun-open-search');
-$aliyun_opensearch = new AliyunOpenSearch(ALI_OPENSEARCH_PLUGIN_NAME, '0.1');
+$aos_version_file = plugin_dir_path(__FILE__) . 'VERSION';
+if (is_file($aos_version_file)) {
+    define('ALI_OPENSEARCH_PLUGIN_VERSION', file_get_contents($aos_version_file));
+} else {
+    define('ALI_OPENSEARCH_PLUGIN_VERSION', 'dev');
+}
+$aliyun_opensearch = new AliyunOpenSearch(ALI_OPENSEARCH_PLUGIN_NAME, ALI_OPENSEARCH_PLUGIN_VERSION);
 $aliyun_opensearch->initialize();
 $frontend = new AliyunOpenSearchFrontend(
     $aliyun_opensearch->getPluginName(),
